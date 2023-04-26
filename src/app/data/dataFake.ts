6a932e2f30d0698183507b96b8efd91134abdfc3
Marcio Farias Pinto <marcio_fpinto@hotmail.com>
2023-04-26 15:30:20 -0300

    Final de projeto blog angular

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "Tudo o que sabemos sobre Hogwarts Legacy",
+        "description": "O Hogwarts Legacy é um RPG de ação de mundo aberto, ambientado no universo de Harry Potter. Você vai interpretar um novo aluno de Hogwarts que chega no quinto ano de estudos. O personagem possui a chave para desvendar um segredo antigo que poderia destruir o mundo bruxo, e tem acesso a uma magia esquecida que ninguém entende. Com a ajuda do Guia de Campo do Bruxo (Wizard's Field Guide), os jogadores irão acelerar seu aprendizado, explorar a Escola de Magia e Bruxaria de Hogwarts, fazer amigos e inimigos e se tornar a bruxa ou bruxo que sempre quiseram ser. ",
+        "photoCover": "https://1.bp.blogspot.com/-X6et1vRHESE/X2J1GpsIksI/AAAAAAAAeB0/BxweFJFrtBYzvXlYD3PhfbF6rPZJoy6qgCLcBGAsYHQ/s1600/hogwarts-legacy-capa.jpg"
+    },
+    {
+        "id": "2",
+        "title": "Data de lançamento de Hogwarts Legacy",
+        "description": "Você pode comprar Hogwarts Legacy na pré-venda agora mesmo na Epic Games Store, e o lançamento completo está marcado para 10 de fevereiro de 2023.",
+        "photoCover": "https://t.ctcdn.com.br/PwZgwYL03Aj0LdoYlvaYi67VLdI=/720x405/smart/filters:format(webp)/i572868.png"
+    },
+    {
+        "id": "3",
+        "title": "Com quem você joga em Hogwarts Legacy?",
+        "description": "Você vai interpretar um novo estudante em Hogwarts, mas um que está ingressando no quinto ano da escola. Não sabemos o motivo desse atraso, mas sabemos que é possível nomear e criar seu personagem. Você também poderá escolher em qual casa vai entrar na hora do teste com o Chapéu Seletor. A escolha entre Grifinória, Lufa-Lufa, Corvinal e Sonserina é só sua.",
+        "photoCover": "https://t.ctcdn.com.br/nS5s3MiR0bUTfAcBOwkg4u9AA7I=/720x405/smart/filters:format(webp)/i575347.jpeg"
+    },
+    {
+        "id": "4",
+        "title": "O que é possível fazer em Hogwarts Legacy?",
+        "description": "Em Hogwarts Legacy, muitos de seus colegas de turma apresentarão histórias adicionais e missões secundárias, algumas com consequências mais sérias, outras nem tanto. Fomos apresentados a Sebastian Sallow, um estudante da Sonserina que adotou as Artes das Trevas, incluindo as Maldições Imperdoáveis, em um esforço para salvar sua irmã. Ele tem uma missão que leva você a lugares sombrios, a quebra-cabeças exclusivos e às batalhas em que você participá. O quanto você vai se envolver com as Artes das Trevas depende só de você, mas você vai ver como isso pode ser devastador para a sua jornada.",
+        "photoCover": "https://cdn2.unrealengine.com/egs-hogwartslegacydigitaldeluxeedition-avalanchesoftware-editions-g1a-00-1920x1080-a95fff2da9f4.jpg"
+    }
+]
